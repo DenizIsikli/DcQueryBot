10db21a5af0a5bfb58d1c166b19ad7f65c48c9c5
Bobby <s215818@dtu.dk>
2023-08-10 21:49:28 +0200

    Fixed bug

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ class Main:
         self.setup_instance_wikipedia = Features.Wikipedia.Setup()
         self.setup_instance_urban_dictionary = Features.UrbanDictionary.Setup()
 
-        # Instance of Main()
-        self.main_instance = Main()
-
     async def setup_all_instances(self, bot):
         setup_instances = [
             self.setup_instance_misc,
@@ -29,15 +26,13 @@ class Main:
         intents_instance = DcQuery.intents_instance
         bot = commands.Bot(command_prefix="!", intents=intents_instance)
 
-        instance_of_main = self.main_instance
-        await instance_of_main.setup_all_instances(bot)
-
         @bot.event
         async def on_ready():
             await bot.change_presence(activity=discord.Game('!help'))
             print(f"Logged in as {bot.user}")
 
         bot_token = "MTEzNjA3MTM5ODk5Mzk2MTEyMg.G3TDmh.cbps9v_FUpdQ6EScMrL7hSJllYuQNpOTeXGmHQ"
+        await self.setup_all_instances(bot)
         await bot.start(bot_token)
 
 
