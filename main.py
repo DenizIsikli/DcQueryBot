5e1c3242e6e9e6e248a6e66c77ab9f2c25e31c1d
Bobby <s215818@dtu.dk>
2023-08-11 03:12:09 +0200

    Run Cogs file with os.listdir to the folder and load extension thereafter

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import discord
 from discord.ext import commands
 import DcQuery
-import Features.Wikipedia
-import Features.UrbanDictionary
 import asyncio
+import os
 
 
 class Main:
     def __init__(self):
-        self.setup_instance_misc = DcQuery.Setup()
-        self.setup_instance_wikipedia = Features.Wikipedia.Setup()
-        self.setup_instance_urban_dictionary = Features.UrbanDictionary.Setup()
+        pass
 
-    async def setup_all_instances(self, bot):
-        setup_instances = [
-            self.setup_instance_misc,
-            self.setup_instance_wikipedia,
-            self.setup_instance_urban_dictionary
-        ]
+    @staticmethod
+    async def setup_all_instances(bot):
+        for filename in os.listdir("Cogs"):
+            if filename.endswith(".py"):
+                cog_name = f"Cogs.{filename}"
+                try:
+                    bot.load_extension(cog_name)
+                    print(f"Loaded cog: {cog_name}")
+                except Exception as e:
+                    print(f"Failed to load cog: {cog_name}: {e}")
 
-        for instance in setup_instances:
-            await instance.setup(bot)
+        try:
+            # Load DcQuery directly
+            bot.load_extension("DcQuery")
+            print("Loaded DcQuery")
+        except Exception as e:
+            print(f"Failed to load DcQuery: {e}")
 
     async def main(self):
         intents_instance = DcQuery.intents_instance
-        bot = commands.Bot(command_prefix="!", intents=intents_instance)
+        bot = commands.Bot(command_prefix="!", owner_id=538816980845854720, intents=intents_instance)
 
         @bot.event
         async def on_ready():
