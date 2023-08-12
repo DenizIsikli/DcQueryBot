8ea2018955cf4f90eac2a9e0f4d96cf64df8b205
Bobby <s215818@dtu.dk>
2023-08-12 14:54:31 +0200

    Added Setup class for setting up cog instances

--- main.py
+++ main.py
@@ -5,31 +5,37 @@ import asyncio
 import os
 
 
-class Main:
+class Setup:
     def __init__(self):
         pass
 
-    @staticmethod
-    async def setup_all_instances(bot):
+
+    async def setup_cog_instances(self, bot):
         for filename in os.listdir("Cogs"):
             if filename.endswith(".py"):
-                cog_name = f"Cogs.{filename}"
+                cog_name = f"Cogs.{filename[:-3]}"  # Remove the last 3 characters (.py)
                 try:
-                    bot.load_extension(cog_name)
-                    print(f"Loaded cog: {cog_name}")
+                    await bot.load_extension(cog_name)
+                    print(f"Loaded Cog File: {cog_name}")
                 except Exception as e:
                     print(f"Failed to load cog: {cog_name}: {e}")
 
         try:
             # Load DcQuery directly
-            bot.load_extension("DcQuery")
-            print("Loaded cog: Cogs.DcQuery.py")
+            await bot.load_extension("DcQuery")
+            print("Loaded Util File: DcQuery")
         except Exception as e:
             print(f"Failed to load DcQuery: {e}")
+
+
+class Main:
+    def __init__(self):
+        self.setup_instance = Setup()
 
     async def main(self):
         intents_instance = DcQuery.intents_instance
         bot = commands.Bot(command_prefix="!", owner_id=538816980845854720, intents=intents_instance)
+        bot.remove_command('help')
 
         @bot.event
         async def on_ready():
@@ -38,7 +44,8 @@ class Main:
             print("_____________________________")
 
         bot_token = "MTEzNjA3MTM5ODk5Mzk2MTEyMg.G3TDmh.cbps9v_FUpdQ6EScMrL7hSJllYuQNpOTeXGmHQ"
-        await self.setup_all_instances(bot)
+        setup_instance = Setup()
+        await setup_instance.setup_cog_instances(bot)
         await bot.start(bot_token)
 
 
