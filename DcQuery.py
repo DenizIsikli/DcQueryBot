28b47444141fae12f7f892514e03fac21d7195be
Bobby <s215818@dtu.dk>
2023-08-12 16:58:20 +0200

    - Added a reference to bot for every command to access the bots functionality and features
    - Fixed reload command, so it shows the reloaded cog files and util file

--- DcQuery.py
+++ DcQuery.py
@@ -2,61 +2,65 @@ import os
 import discord
 import datetime
 from discord.ext import commands
-import main
-from dataclasses import dataclass
-
-
-@dataclass
-class Baseclass:
-    intents_instance = main.Intents
 
 
 class HelpEmbed(commands.Cog):
-    @commands.command(intents=Baseclass.intents_instance)
-    async def help(self, ctx, message: discord.Message):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def help(self, ctx):
         embed = discord.Embed(
             title="Command List",
             color=discord.Color.purple()
         )
         embed.add_field(
                         name="**!wiki**",
-                        value="!wiki {int: article amount} {search query}\nExample: *!wiki Github* || *!wiki 2 Github*"
+                        value="!wiki {int: article amount} {search query}\nExample: *!wiki Github* || *!wiki 2 Github*",
+                        inline=False
         )
         embed.add_field(
             name="**!urban**",
-            value="!urban {int: word amount} {search query}\nExample: *!urban Shookie* || *!urban 2 Shookie*"
+            value="!urban {int: word amount} {search query}\nExample: *!urban Shookie* || *!urban 2 Shookie*",
+            inline=False
         )
 
         # Add & set footer with timestamp
         timestamp = datetime.datetime.utcnow()
         embed.timestamp = timestamp
-        embed.set_footer(text=f"Requested by {message.author.name}")
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
 
         await ctx.send(embed=embed)
 
 
 class AdminEmbed(commands.Cog):
-    @commands.command(intents=Baseclass.intents_instance)
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
     @commands.is_owner()
-    async def admin(self, ctx, message: discord.Message):
+    async def admin(self, ctx):
         embed = discord.Embed(
             title="Admin Command List",
             color=discord.Color.orange()
         )
         embed.add_field(name="**!delete**", value="*Delete x amount of channel messages: default amount = 1*")
-        embed.add_field(name="**!kick**", value="*Kick specified member: reason[optional]")
+        embed.add_field(name="**!kick**", value="*Kick specified member: reason[optional]*")
         embed.add_field(name="**!reload**", value="*Reload all Cog files*")
 
         # Add & set footer with timestamp
         timestamp = datetime.datetime.utcnow()
         embed.timestamp = timestamp
-        embed.set_footer(text=f"Requested by {message.author.name}")
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
 
         await ctx.send(embed=embed)
 
 
 class AdminDelete(commands.Cog):
-    @commands.command(intents=Baseclass.intents_instance)
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
     @commands.is_owner()
     async def delete(self, ctx, amount=1):
         await ctx.channel.purge(limit=amount+1)
@@ -64,14 +68,17 @@ class AdminDelete(commands.Cog):
     @delete.error
     async def delete_error(self, error, ctx):
         if isinstance(error, commands.MissingPermissions):
-            command_owner = commands.is_owner()
+            command_owner = ctx.bot.get_user(ctx.bot.owner_id)
             await ctx.send(f"Missing permission! - Permission assigned to {command_owner}")
         elif isinstance(error, commands.BadArgument):
             await ctx.send("Bad argument")
 
 
 class Kick(commands.Cog):
-    @commands.command(intents=Baseclass.intents_instance)
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
     @commands.is_owner()
     async def kick(self, member, *, reason=None):
         await member.kick(reason=reason)
@@ -79,19 +86,19 @@ class Kick(commands.Cog):
     @kick.error
     async def kick_error(self, error, ctx):
         if isinstance(error, commands.MissingPermissions):
-            command_owner = commands.is_owner()
+            command_owner = ctx.bot.get_user(ctx.bot.owner_id)
             await ctx.send(f"Missing permission! - Permission assigned to {command_owner}")
         elif isinstance(error, commands.BadArgument):
             await ctx.send("Bad argument")
 
 
 class Reload(commands.Cog):
-    def __init__(self):
-        self.bot = commands.Bot
+    def __init__(self, bot):
+        self.bot = bot
 
-    @commands.command(intents=Baseclass.intents_instance)
+    @commands.command()
     @commands.is_owner()
-    async def reload(self, ctx, bot, message: discord.Message):
+    async def reload(self, ctx):
         async with ctx.typing():
             embed = discord.Embed(
                 title="Reloading all cogs!",
@@ -101,34 +108,54 @@ class Reload(commands.Cog):
             # Add & set footer with timestamp
             timestamp = datetime.datetime.utcnow()
             embed.timestamp = timestamp
-            embed.set_footer(text=f"Requested by {message.author.name}")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+
+            # List for reloaded cogs
+            reloaded_cogs = []
+            reloaded_util = []
 
             try:
                 for filename in os.listdir("Cogs"):
                     if filename.endswith(".py"):
+                        cog_name = f"Cogs.{filename[:-3]}"  # Remove the last 3 characters (.py)
                         try:
-                            await bot.unload_extension(f"Cogs.{filename}")
-                            await bot.load_extension(f"Cogs.{filename}")
-                            embed.add_field(
-                                name=f"Reloaded: {filename}",
-                                value='\uFeFF'
-                            )
+                            await self.bot.unload_extension(cog_name)
+                            await self.bot.load_extension(cog_name)
+                            reloaded_cogs.append(cog_name)  # Add file to list
                         except Exception as e:
-                            print(f"Failed to reload cog: Cogs.{filename}: {e}")
+                            print(f"Failed to reload cog files: Cogs.{cog_name}: {e}")
+                try:
+                    await self.bot.unload_extension("DcQuery")
+                    await self.bot.load_extension("DcQuery")
+                    reloaded_util.append("DcQuery")
+                except Exception as e:
+                    print(f"Failed to reload util file: DcQuery: {e}")
+
+
+
+                if reloaded_cogs:
+                    embed.add_field(
+                        name="Reloaded Cogs Files",
+                        value="\n".join(reloaded_cogs)
+                    )
+                    embed.add_field(
+                        name="Reloaded Util File",
+                        value="\n".join(reloaded_util)
+                    )
             except Exception as e:
                 print(f"Failed to find the cog files: {e}")
 
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 
 async def setup(bot):
     # Util commands
-    await bot.add_cog(Reload())
+    await bot.add_cog(Reload(bot))
 
     # Admin commands
-    await bot.add_cog(AdminDelete())
-    await bot.add_cog(Kick())
+    await bot.add_cog(AdminDelete(bot))
+    await bot.add_cog(Kick(bot))
 
     # Embed commands
-    await bot.add_cog(HelpEmbed())
-    await bot.add_cog(AdminEmbed())
+    await bot.add_cog(HelpEmbed(bot))
+    await bot.add_cog(AdminEmbed(bot))
