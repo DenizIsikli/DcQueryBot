54bf9fe7d2a463ad6b8248cbaab615d8d2030499
Bobby <s215818@dtu.dk>
2023-08-16 00:40:20 +0200

    Refactored and removed HelpEmbed - file is exclusively for admin utils

--- AdminUtils.py
+++ AdminUtils.py
@@ -1,36 +1,7 @@
 import os
 import discord
-import datetime
 from discord.ext import commands
-
-
-class HelpEmbed(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def help(self, ctx):
-        embed = discord.Embed(
-            title="Command List",
-            color=discord.Color.purple()
-        )
-        embed.add_field(
-                        name="**!wiki**",
-                        value="!wiki {int: article amount} {search query}\nExample: *!wiki Github* || *!wiki 2 Github*",
-                        inline=False
-        )
-        embed.add_field(
-            name="**!urban**",
-            value="!urban {int: word amount} {search query}\nExample: *!urban Shookie* || *!urban 2 Shookie*",
-            inline=False
-        )
-
-        # Add & set footer with timestamp
-        timestamp = datetime.datetime.utcnow()
-        embed.timestamp = timestamp
-        embed.set_footer(text=f"Requested by {ctx.author.name}")
-
-        await ctx.send(embed=embed)
+import datetime
 
 
 class AdminEmbed(commands.Cog):
@@ -157,5 +128,4 @@ async def setup(bot):
     await bot.add_cog(Kick(bot))
 
     # Embed commands
-    await bot.add_cog(HelpEmbed(bot))
     await bot.add_cog(AdminEmbed(bot))
