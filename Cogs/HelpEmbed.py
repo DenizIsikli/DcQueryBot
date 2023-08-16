df9b12ab44619b10962ead614f6d6ea0f8287cf8
Bobby <s215818@dtu.dk>
2023-08-17 01:41:00 +0200

    Formatting

--- Cogs/HelpEmbed.py
+++ Cogs/HelpEmbed.py
@@ -15,32 +15,39 @@ class HelpEmbed(commands.Cog):
         )
         embed.add_field(
                         name="**!wiki**",
-                        value="!wiki {int: article amount} {str: search query}\nExample: *!wiki Github* || *!wiki 2 Github*",
+                        value="!wiki {int: article amount} {str: search query}\n"
+                              "Example: *!wiki Github* || *!wiki 2 Github*",
                         inline=False
         )
         embed.add_field(
             name="**!urban**",
-            value="!urban {int: word amount} {str: search query}\nExample: *!urban Shookie* || *!urban 2 Shookie*",
+            value="!urban {int: word amount} {str: search query}\n"
+                  "Example: *!urban Shookie* || *!urban 2 Shookie*",
             inline=False
         )
         embed.add_field(
             name="**!qrcode**",
-            value="!qrcode {link}\nExample: *!qrcode GitHub.com*",
+            value="!qrcode {link}\n"
+                  "Example: *!qrcode GitHub.com*",
             inline=False
         )
         embed.add_field(
             name="**!imgur**",
-            value="!imgur {str: search query}\nExample: *!imgur cat*",
+            value="!imgur {str: search query}\n"
+                  "Example: *!imgur cat*",
             inline=False
         )
         embed.add_field(
             name="**!ascii**",
-            value="!ascii {str: text}\nExample: *!ascii placeholder*",
+            value="!ascii {str: text}\n"
+                  "Example: *!ascii placeholder*",
             inline=False
         )
         embed.add_field(
             name="**!asciicm**",
-            value="!asciicm {str: text}\nExample: *!asciicm placeholder*\n__asciicm does not take numbers as an argument, only text__"
+            value="!asciicm {str: text}\n"
+                  "Example: *!asciicm placeholder*\n"
+                  "`asciicm does not take numbers as an argument, only text`"
         )
 
         # ** ADD NEW COMMANDS TO EMBED **
