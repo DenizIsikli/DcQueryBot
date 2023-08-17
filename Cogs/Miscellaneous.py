a59449268a0772ae8fa3a54d0fc91323b5f1df14
Bobby <s215818@dtu.dk>
2023-08-17 19:14:35 +0200

    Minor changes

--- Cogs/Miscellaneous.py
+++ Cogs/Miscellaneous.py
@@ -18,25 +18,23 @@ class Miscellaneous(commands.Cog):
             title=f"Who is {member.display_name}",
             color=discord.Color.dark_theme()
         )
-        embed.set_thumbnail(url=member.avatar)
+        embed.set_thumbnail(url=member.avatar_url)
 
-        embed.add_field(name="**Name**", value=member.display_name, inline=False)
-        embed.add_field(name="**Account Created**", value=member.created_at, inline=False)
+        embed.add_field(name="**ID:**", value=member.id)
+        embed.add_field(name="**Name:**", value=member.display_name)
 
-        if member.joined_at:
-            joined_time = datetime.datetime.now() - member.joined_at
-            embed.add_field(name="**Time in Server**", value=str(joined_time), inline=False)
+        embed.add_field(name="**Created Account On:**", value=member.created_at)
+        embed.add_field(name="**Joined Server On:**", value=member.joined_at)
 
-        roles = ", ".join([role.mention for role in member.roles if role != ctx.guild.default_role])
-        embed.add_field(name="**Roles**", value=roles, inline=False)
+        embed.add_field(name="**Roles:**", value=member.roles)
+        embed.add_field(name="**Highest Role:", value=member.top_role)
 
         # Add & set footer with timestamp
         timestamp = datetime.datetime.utcnow()
         embed.timestamp = timestamp
-        embed.set_footer(text=f"Requested by {ctx.author.name}")
+        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
-        print("embed sent")
 
     @commands.command()
     async def whois(self, ctx, member: discord.Member = None):
