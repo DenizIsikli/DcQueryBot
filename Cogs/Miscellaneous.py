3276acfa1be4e52edc1ef8ec7094fc6285c6ab7f
Bobby <s215818@dtu.dk>
2023-08-19 01:35:23 +0200

    ChangeNickname

--- Cogs/Miscellaneous.py
+++ Cogs/Miscellaneous.py
@@ -2,8 +2,10 @@ import os
 import discord
 from discord.ext import commands
 import datetime
-from gtts import gTTS
-from mutagen.mp3 import MP3
+import aiohttp
+import asyncio
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 
 class WhoIs(commands.Cog):
@@ -57,37 +59,78 @@ class TextToSpeech(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @staticmethod
-    async def text_to_speech(ctx, *, text: str = None):
+        # Initialize the IBM Watson Text to Speech client
+        authenticator = IAMAuthenticator('YOUR_API_KEY')  # Replace with your API key
+        self.client = TextToSpeechV1(authenticator=authenticator)
+        self.client.set_service_url('YOUR_SERVICE_URL')  # Replace with your service URL
+
+    async def text_to_speech(self, ctx, *, text: str = None):
+        global vc
         try:
             if ctx.author.voice and ctx.author.voice.channel:
-                speech = gTTS(text=text, lang="en", slow=False, lang_check=True)
-                speech.save("speech.mp3")
-                audio = MP3("speech.mp3")
-                audio_length = audio.info.length
+                if text is not None and text.strip() != "":
+                    response = self.client.synthesize(text, accept='audio/mp3')
 
-                if audio_length > 0:
+                    # Save the audio to a file
+                    with open("speech.mp3", "wb") as out_file:
+                        out_file.write(response.get_result().content)
+
                     voice_channel = ctx.author.voice.channel
                     vc = await voice_channel.connect()
-                    vc.play(discord.FFmpegPCMAudio("speech.mp3"))
+
+                    vc.play(discord.FFmpegPCMAudio("speech.mp3"), after=lambda e: os.remove("speech.mp3"))
+
+                    while vc.is_playing():
+                        await asyncio.sleep(1)
+
                     await vc.disconnect()
-                    os.remove("speech.mp3")
                 else:
-                    await ctx.send("Failed to generate audio!")
+                    await ctx.send("Please provide the text you want to convert to speech.")
             else:
                 await ctx.send("You need to be inside a voice channel to use this command!")
         except Exception as e:
             await ctx.send(f"Failed to save the text to audio: {e}")
+            await asyncio.sleep(1)
+            await vc.disconnect()
             os.remove("speech.mp3")
 
     @commands.command()
-    async def tts(self, ctx, text: str = None):
+    async def tts(self, ctx, *, text: str = None):
         await self.text_to_speech(ctx, text=text)
 
     @tts.error
     async def tts_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please provide the text you want to convert to speech.")
+        elif isinstance(error, commands.CommandInvokeError):
+            await ctx.send("An error occurred while processing your request.")
+        elif isinstance(error, aiohttp.ClientOSError):
+            await ctx.send("An error occurred while downloading the audio.")
+        else:
+            await ctx.send("An error occurred.")
+
+
+class ChangeNickname(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def change_nickname(ctx, *, new_name: str = None):
+        try:
+            old_name = ctx.author.display_name
+            await ctx.author.edit(nick=new_name)
+            await ctx.send(f"Nickname changed from {old_name} to {new_name}")
+        except discord.Forbidden:
+            await ctx.send("I don't have permission to change nicknames.")
+
+    @commands.command()
+    async def nick(self, ctx, *, new_name: str = None):
+        await self.change_nickname(ctx, new_name=new_name)
+
+    @nick.error
+    async def nick_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please provide the new nickname you want to set.")
         elif isinstance(error, commands.CommandInvokeError):
             await ctx.send("An error occurred while processing your request.")
         else:
