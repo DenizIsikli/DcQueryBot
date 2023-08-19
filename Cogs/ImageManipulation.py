391e57265f588b153f0213403cc9572e35e9ea2f
Bobby <s215818@dtu.dk>
2023-08-20 00:19:46 +0200

    Error handling

--- Cogs/ImageManipulation.py
+++ Cogs/ImageManipulation.py
@@ -24,25 +24,34 @@ class ImageManipulation(commands.Cog):
 
             # Send the filtered image
             await ctx.send(file=discord.File(img_byte_array, "BlurredFilter_image.png"))
+
         except IndexError:
             await ctx.send("Please provide an image as an attachment.")
+
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
 
     @staticmethod
     async def apply_sharpen(ctx):
-        image = ctx.message.attachments[0]
+        try:
+            image = ctx.message.attachments[0]
 
-        # Open the image using PIL
-        image = Image.open(image.fp)
-        filtered_image = image.filter(ImageFilter.SHARPEN)
+            # Open the image using PIL
+            image = Image.open(image.fp)
+            filtered_image = image.filter(ImageFilter.SHARPEN)
 
-        # Convert the PIL Image to bytes since discord.File doesn't take "Image"
-        img_byte_array = io.BytesIO()
-        filtered_image.save(img_byte_array, format="PNG")
+            # Convert the PIL Image to bytes since discord.File doesn't take "Image"
+            img_byte_array = io.BytesIO()
+            filtered_image.save(img_byte_array, format="PNG")
 
-        # Send the filtered image
-        await ctx.send(file=discord.File(img_byte_array, "SharpenFilter_image.png"))
+            # Send the filtered image
+            await ctx.send(file=discord.File(img_byte_array, "SharpenFilter_image.png"))
+
+        except IndexError:
+            await ctx.send("Please provide an image as an attachment.")
+
+        except Exception as e:
+            await ctx.send(f"An error occurred: {e}")
 
 
 async def setup(bot):
