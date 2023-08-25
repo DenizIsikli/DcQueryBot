8b336d01b5b881eab63e0311da13d37ebcf5d0b9
Bobby <s215818@dtu.dk>
2023-08-25 18:59:36 +0200

    Need another api

--- ApiQueries/ChatGPT.py
+++ ApiQueries/ChatGPT.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 import aiohttp
 from discord.ext import commands
@@ -6,7 +7,7 @@ from discord.ext import commands
 class ChatGPT(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.CHAT_GPT_API = "https://open-ai21.p.rapidapi.com/conversationllama"
+        self.CHAT_GPT_API = "https://chatgpt9.p.rapidapi.com/api/chat-gpt/completions"
 
     async def chat_gpt(self, ctx, *, content: str = None):
         if ctx.author == ctx.bot.user:
@@ -20,35 +21,27 @@ class ChatGPT(commands.Cog):
             headers = {
                 "content-type": "application/json",
                 "X-RapidAPI-Key": "2ba10896fdmsh6eb24b198a7b520p1fef74jsneb8afa07df45",
-                "X-RapidAPI-Host": "open-ai21.p.rapidapi.com"
+                "X-RapidAPI-Host": "chatgpt9.p.rapidapi.com"
             }
 
             payload = {
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": content
-                    }
-                ],
-                "web_access": False
+                "model": "text-davinci-003",
+                "prompt": content,
+                "temperature": 0,
+                "max_tokens": 7
             }
 
             async with aiohttp.ClientSession() as session:
-                async with session.get(self.CHAT_GPT_API, json=payload, headers=headers) as response:
+                async with session.post(self.CHAT_GPT_API, json=payload, headers=headers) as response:
                     response.raise_for_status()  # Raise an exception if the request was not successful
                     response_data = await response.json()
-
-                    ai_response = response_data.get('LLAMA')  # Extract the AI response
-                    if ai_response:
-                        await ctx.send(f"ChatGPT: {ai_response[:40]}")
-                    else:
-                        await ctx.send("The AI couldn't provide a response.")
+                    print(response.content_type)
 
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
 
     @commands.command()
-    async def chat(self, ctx, *, content: str = None):
+    async def gpt(self, ctx, *, content: str = None):
         await self.chat_gpt(ctx, content=content)
 
 
