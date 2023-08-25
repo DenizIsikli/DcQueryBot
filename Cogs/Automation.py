c28c114cc5b670c5236e0694e6e92b4ad6211649
Bobby <s215818@dtu.dk>
2023-08-25 18:51:51 +0200

    Automation.py

---
+++ Cogs/Automation.py
@@ -0,0 +1,44 @@
+import asyncio
+import random
+import keyboard
+import pyautogui
+from discord.ext import commands
+
+
+class AfkAutomation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.i = 0
+
+    async def afk_automation(self, ctx, timer: int = 60):
+        auto = pyautogui
+        auto.FAILSAFE = False
+        x, y = auto.size()
+
+        async def loop_function():
+            while self.i < timer:
+                x1 = random.randint(0, x)
+                y1 = random.randint(0, y)
+
+                auto.moveTo(x1, y1)
+
+                await asyncio.sleep(1)
+                self.i += 1
+
+                if keyboard.is_pressed("f"):
+                    return True
+
+            return False
+
+        if await loop_function():
+            await ctx.send(f"AFK automation stopped by {ctx.author.mention}")
+        else:
+            await ctx.send(f"Your timer is done: {ctx.author.mention}")
+
+    @commands.command()
+    async def afk(self, ctx, timer: int = 60):
+        await self.afk_automation(ctx, timer)
+
+
+async def setup(bot):
+    await bot.add_cog(AfkAutomation(bot))
