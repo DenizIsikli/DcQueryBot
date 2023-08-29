3f28e6867db38ba9f37841be88b6d13bd0e4bcf1
Bobby <s215818@dtu.dk>
2023-08-29 21:19:14 +0200

    Fixed

--- ApiQueries/ChatGPT.py
+++ ApiQueries/ChatGPT.py
@@ -1,5 +1,3 @@
-import json
-import asyncio
 import aiohttp
 from discord.ext import commands
 
@@ -7,7 +5,7 @@ from discord.ext import commands
 class ChatGPT(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.CHAT_GPT_API = "https://chatgpt9.p.rapidapi.com/api/chat-gpt/completions"
+        self.CHAT_GPT_API = "https://simple-chatgpt-api.p.rapidapi.com/ask"
 
     async def chat_gpt(self, ctx, *, content: str = None):
         if ctx.author == ctx.bot.user:
@@ -21,21 +19,19 @@ class ChatGPT(commands.Cog):
             headers = {
                 "content-type": "application/json",
                 "X-RapidAPI-Key": "2ba10896fdmsh6eb24b198a7b520p1fef74jsneb8afa07df45",
-                "X-RapidAPI-Host": "chatgpt9.p.rapidapi.com"
+                "X-RapidAPI-Host": "simple-chatgpt-api.p.rapidapi.com"
             }
 
-            payload = {
-                "model": "text-davinci-003",
-                "prompt": content,
-                "temperature": 0,
-                "max_tokens": 7
-            }
+            payload = {"question": content}
 
             async with aiohttp.ClientSession() as session:
                 async with session.post(self.CHAT_GPT_API, json=payload, headers=headers) as response:
                     response.raise_for_status()  # Raise an exception if the request was not successful
                     response_data = await response.json()
-                    print(response.content_type)
+
+                    ai_response = response_data.get('answer', 'No response from the AI')
+
+                    await ctx.send(ai_response)
 
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
