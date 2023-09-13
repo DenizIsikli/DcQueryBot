63c9e6c415fa0f1eb8aa56e54efb9988c9a1b53a
Bobby <s215818@dtu.dk>
2023-09-13 23:59:00 +0200

    Language code

--- Cogs/Miscellaneous.py
+++ Cogs/Miscellaneous.py
@@ -177,25 +177,155 @@ class TranslateText(commands.Cog):
         self.bot = bot
         self.translator = Translator()
 
+        self.count = 0
+
     async def translater(self, ctx, target_lang: str = None, *, text: str = None):
         if ctx.author == ctx.bot.user:
             return
 
         if target_lang is None:
             await ctx.send("Specify a language.")
+            return
+
+        if text is None:
+            await ctx.send("Specify text to translate.")
+            return
 
         try:
             if target_lang in LANGUAGES:
                 translation = self.translator.translate(text, dest=target_lang)
                 await ctx.send(f"Translation to {LANGUAGES[target_lang]}: {translation.text}")
             else:
-                await ctx.send("Invalid target language. Please provide a valid language code.")
+                await ctx.send("Invalid target language. Please provide a valid language code: !languagecode")
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
 
     @commands.command()
     async def translate(self, ctx, target_lang: str = None, *, text: str = None):
         await self.translater(ctx, target_lang, text=text)
+
+    @commands.command()
+    async def languagecode(self, ctx):
+        LANGUAGES = {
+            'af': 'afrikaans',
+            'sq': 'albanian',
+            'am': 'amharic',
+            'ar': 'arabic',
+            'hy': 'armenian',
+            'az': 'azerbaijani',
+            'eu': 'basque',
+            'be': 'belarusian',
+            'bn': 'bengali',
+            'bs': 'bosnian',
+            'bg': 'bulgarian',
+            'ca': 'catalan',
+            'ceb': 'cebuano',
+            'ny': 'chichewa',
+            'zh-cn': 'chinese (simplified)',
+            'zh-tw': 'chinese (traditional)',
+            'co': 'corsican',
+            'hr': 'croatian',
+            'cs': 'czech',
+            'da': 'danish',
+            'nl': 'dutch',
+            'en': 'english',
+            'eo': 'esperanto',
+            'et': 'estonian',
+            'tl': 'filipino',
+            'fi': 'finnish',
+            'fr': 'french',
+            'fy': 'frisian',
+            'gl': 'galician',
+            'ka': 'georgian',
+            'de': 'german',
+            'el': 'greek',
+            'gu': 'gujarati',
+            'ht': 'haitian creole',
+            'ha': 'hausa',
+            'haw': 'hawaiian',
+            'iw': 'hebrew',
+            'he': 'hebrew',
+            'hi': 'hindi',
+            'hmn': 'hmong',
+            'hu': 'hungarian',
+            'is': 'icelandic',
+            'ig': 'igbo',
+            'id': 'indonesian',
+            'ga': 'irish',
+            'it': 'italian',
+            'ja': 'japanese',
+            'jw': 'javanese',
+            'kn': 'kannada',
+            'kk': 'kazakh',
+            'km': 'khmer',
+            'ko': 'korean',
+            'ku': 'kurdish (kurmanji)',
+            'ky': 'kyrgyz',
+            'lo': 'lao',
+            'la': 'latin',
+            'lv': 'latvian',
+            'lt': 'lithuanian',
+            'lb': 'luxembourgish',
+            'mk': 'macedonian',
+            'mg': 'malagasy',
+            'ms': 'malay',
+            'ml': 'malayalam',
+            'mt': 'maltese',
+            'mi': 'maori',
+            'mr': 'marathi',
+            'mn': 'mongolian',
+            'my': 'myanmar (burmese)',
+            'ne': 'nepali',
+            'no': 'norwegian',
+            'or': 'odia',
+            'ps': 'pashto',
+            'fa': 'persian',
+            'pl': 'polish',
+            'pt': 'portuguese',
+            'pa': 'punjabi',
+            'ro': 'romanian',
+            'ru': 'russian',
+            'sm': 'samoan',
+            'gd': 'scots gaelic',
+            'sr': 'serbian',
+            'st': 'sesotho',
+            'sn': 'shona',
+            'sd': 'sindhi',
+            'si': 'sinhala',
+            'sk': 'slovak',
+            'sl': 'slovenian',
+            'so': 'somali',
+            'es': 'spanish',
+            'su': 'sundanese',
+            'sw': 'swahili',
+            'sv': 'swedish',
+            'tg': 'tajik',
+            'ta': 'tamil',
+            'te': 'telugu',
+            'th': 'thai',
+            'tr': 'turkish',
+            'uk': 'ukrainian',
+            'ur': 'urdu',
+            'ug': 'uyghur',
+            'uz': 'uzbek',
+            'vi': 'vietnamese',
+            'cy': 'welsh',
+            'xh': 'xhosa',
+            'yi': 'yiddish',
+            'yo': 'yoruba',
+            'zu': 'zulu',
+        }
+
+        language_code = "**Language Code**\n"
+
+        for code, language in LANGUAGES.items():
+            language_code += f"- {code}: {language}\n"
+            self.count += 1
+
+            if self.count % 5 == 0:
+                language_code += "\n"
+
+        await ctx.send(f"{language_code}")
 
 
 class GitHub(commands.Cog):
