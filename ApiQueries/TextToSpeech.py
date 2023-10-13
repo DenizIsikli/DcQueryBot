de0e6eca101628035c1429a9a1b92a3d354164db
Bobby <s215818@dtu.dk>
2023-10-13 23:45:51 +0200

    Minor changes

--- ApiQueries/TextToSpeech.py
+++ ApiQueries/TextToSpeech.py
@@ -8,12 +8,12 @@ import aiohttp
 class TextToSpeech(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.TEXTTOSPEECH_API = "https://text-to-speech-for-28-languages.p.rapidapi.com/"
+        self.TEXTTOSPEECH_API = "https://text-to-speech27.p.rapidapi.com/speech"
 
     async def text_to_speech(self, ctx, vc, *, content: str = ""):
         if ctx.author == ctx.bot.user:
             return
-        print("hi im here")
+
         if ctx.author.voice is None:
             await ctx.send("You need to be inside a voice channel to use this command.")
             return
@@ -24,35 +24,26 @@ class TextToSpeech(commands.Cog):
 
         try:
             headers = {
-                "content-type": "application/x-www-form-urlencoded",
                 "X-RapidAPI-Key": "2ba10896fdmsh6eb24b198a7b520p1fef74jsneb8afa07df45",
-                "X-RapidAPI-Host": "text-to-speech-for-28-languages.p.rapidapi.com"
+                "X-RapidAPI-Host": "text-to-speech27.p.rapidapi.com"
             }
 
-            payload = {
-                "msg": content,
-                "lang": "Salli",
-                "source": "ttsmp3"
-            }
+            querystring = {"text": f"{content}", "lang": "en-us"}
 
-            print("before post")
             async with aiohttp.ClientSession() as session:
-                async with session.post(self.TEXTTOSPEECH_API, data=payload, headers=headers) as response:
+                async with session.get(self.TEXTTOSPEECH_API, headers=headers, params=querystring) as response:
                     response.raise_for_status()  # Raise an exception if the request was not successful
                     response_data = await response.json()
-
                     print(response_data)
-                    print(response.content_type)
+                    print("test")
 
-                    # audio_data = response_data["audioContent"]
-                    # audio_filename = "tts_audio.ogg"
-                    # with open(audio_filename, "wb") as audio_file:
-                    #     audio_file.write(audio_data)
-                    #
-                    # if vc is not None and not vc.is_playing():
-                    #     await asyncio.sleep(3)
-                    #     await ctx.message.delete()
-                    #     vc.play(FFmpegPCMAudio(audio_filename))
+                    vc = ctx.author.voice.channel
+                    await vc.connect()
+
+                    vc.play(FFmpegPCMAudio(response_data["audio_url"]))
+                    while vc.is_playing():
+                        await asyncio.sleep(1)
+                    await vc.disconnect()
 
         except Exception as e:
             await ctx.send(f"Failed to save the text to audio: {e}")
